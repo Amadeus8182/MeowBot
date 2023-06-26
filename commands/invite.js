5dbb231325ed01aa81984d0f555a0772d5d185f3
Amadeus8182 <amadeus8182@gmail.com>
2023-06-26 17:37:22 -0500

    change message to be inside of an embed with a hyperlink

--- commands/invite.js
+++ commands/invite.js
@@ -1,8 +1,12 @@
+const { EmbedBuilder } = require("@discordjs/builders")
 module.exports = {
     name: 'invite',
 	desc: 'sends an invite link for the bot to be able to join other servers.',
     execute(msg) {
-        msg.channel.send('You can invite using this link! https://discord.com/oauth2/authorize?client_id=1066857245675102360&scope=bot&permissions=274878032960');
+		let embed = new EmbedBuilder()
+			.setTitle('Invite Meowbot')
+			.setDescription('You can invite MeowBot to your own server using this [link](https://discord.com/oauth2/authorize?client_id=1104286435365900288&scope=bot&permissions=8)!');
+        msg.channel.send({embeds: [embed]});
     }
 
 }
