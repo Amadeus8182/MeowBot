f76e53cdd008bf58fa33457e5c75319499c6127f
Amadeus8182 <amadeus8182@gmail.com>
2023-07-20 07:21:35 -0500

    unecessary async

--- helpers/update_points.js
+++ helpers/update_points.js
@@ -3,7 +3,7 @@ const db = new sqlite3.Database('./users.db');
 
 module.exports = {
 	name:'update_points',
-	async execute(id, rate) {
+	execute(id, rate) {
 		db.run(`UPDATE user_info SET points = points + ${rate} WHERE id = ${id}`, (err) => {
 			if(err) return console.log(err + ' ' + this.name);
 		});
