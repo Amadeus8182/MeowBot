56ebcdaf2593a2c63182321ce93ab2619b229a40
Amadeus8182 <amadeus8182@gmail.com>
2023-07-21 02:21:30 -0500

    added three other commands that give points
    also fix description on plunder

--- commands/pillage.js
+++ commands/pillage.js
@@ -4,23 +4,23 @@ const update_cooldown = require('../helpers/update_cooldown.js');
 const get_user_info = require('../helpers/get_user_info.js');
 
 module.exports = {
-	name: 'plunder',
-	desc: 'earn meow meow points every hour',
+	name: 'pillage',
+	desc: 'earn meow meow points every twelve hours',
 	async execute(msg) {
-		const POINTS_GIVEN = 2000 + Math.floor(Math.random()*500);
-		const COOLDOWN = 3600;	// cooldown has to be written in seconds
+		const POINTS_GIVEN = 18000 + Math.floor(Math.random()*6000);
+		const COOLDOWN = 12*3600;	// cooldown has to be written in seconds
 		const USER_ID = msg.author.id;
 		const USERNAME = msg.author.username;
 
 		await check_user.execute(USER_ID, USERNAME)
-		let info = await get_user_info.execute(USER_ID, ['plunder_next_used'])
+		let info = await get_user_info.execute(USER_ID, ['pillage_next_used'])
 
 		let seconds_since_epoch = Math.floor(new Date() / 1000);
 
-		if(info.plunder_next_used <= seconds_since_epoch) {
+		if(info.pillage_next_used <= seconds_since_epoch) {
 			update_points.execute(USER_ID, POINTS_GIVEN);
 			update_cooldown.execute(USER_ID, this.name, COOLDOWN);
-			msg.channel.send(`You plundered ${POINTS_GIVEN} points!`);
+			msg.channel.send(`You pillaged ${POINTS_GIVEN} points!`);
 		} else {
 			msg.channel.send(`This command is on cooldown!`);
 		}
--- commands/raid.js
+++ commands/raid.js
@@ -4,23 +4,23 @@ const update_cooldown = require('../helpers/update_cooldown.js');
 const get_user_info = require('../helpers/get_user_info.js');
 
 module.exports = {
-	name: 'plunder',
-	desc: 'earn meow meow points every hour',
+	name: 'raid',
+	desc: 'earn meow meow points every six hours',
 	async execute(msg) {
-		const POINTS_GIVEN = 2000 + Math.floor(Math.random()*500);
-		const COOLDOWN = 3600;	// cooldown has to be written in seconds
+		const POINTS_GIVEN = 10000 + Math.floor(Math.random()*2000);
+		const COOLDOWN = 6*3600;	// cooldown has to be written in seconds
 		const USER_ID = msg.author.id;
 		const USERNAME = msg.author.username;
 
 		await check_user.execute(USER_ID, USERNAME)
-		let info = await get_user_info.execute(USER_ID, ['plunder_next_used'])
+		let info = await get_user_info.execute(USER_ID, ['raid_next_used'])
 
 		let seconds_since_epoch = Math.floor(new Date() / 1000);
 
-		if(info.plunder_next_used <= seconds_since_epoch) {
+		if(info.raid_next_used <= seconds_since_epoch) {
 			update_points.execute(USER_ID, POINTS_GIVEN);
 			update_cooldown.execute(USER_ID, this.name, COOLDOWN);
-			msg.channel.send(`You plundered ${POINTS_GIVEN} points!`);
+			msg.channel.send(`You raided ${POINTS_GIVEN} points!`);
 		} else {
 			msg.channel.send(`This command is on cooldown!`);
 		}
