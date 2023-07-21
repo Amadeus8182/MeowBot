0f9c3d92aa9166648bfee9ab3a691e64ee2dceae
Amadeus8182 <amadeus8182@gmail.com>
2023-07-21 02:14:15 -0500

    remove unecessary require to sqlite3 for db access

--- commands/plunder.js
+++ commands/plunder.js
@@ -3,9 +3,6 @@ const update_points = require('../helpers/update_points.js');
 const update_cooldown = require('../helpers/update_cooldown.js');
 const get_user_info = require('../helpers/get_user_info.js');
 
-const sqlite3 = require('sqlite3');
-const db = new sqlite3.Database('./users.db');
-
 module.exports = {
 	name: 'plunder',
 	desc: 'earn meow meow points every 12 hours',
